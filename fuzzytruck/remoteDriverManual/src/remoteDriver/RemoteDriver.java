142c4cb3fb37863e6abaf6b4449b3f8f0cabdc4d
bonotto <bruno_mcl@hotmail.com.br>
2018-11-01 19:57:13 -0300

    Final work os AI initialization.

---
+++ fuzzytruck/remoteDriverManual/src/remoteDriver/RemoteDriver.java
@@ -0,0 +1,87 @@
+package remoteDriver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.StringTokenizer;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+ 
+public class RemoteDriver {
+	
+	static int port = 4321;
+	static String host = "localhost";
+	
+    public static void main(String[] args) throws IOException {
+        	    	
+        Socket kkSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+ 
+        try {
+            kkSocket = new Socket(host, port);
+            out = new PrintWriter(kkSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host:"  + host);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: " + host);
+            System.exit(1);
+        }
+ 
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String fromServer;
+ 
+        double x, y;
+        double angle;
+        
+        // requisicao da posicao do caminhao
+        out.println("r");
+        
+        String filename = "/home/bonotto/Dropbox/Faculdade/2018-02/INE5430/fuzzytruck/remoteDriverManual/src/remoteDriver/truckBrunoJoaoSouto.fcl";
+        
+        FIS fis = FIS.load(filename,true);
+        
+        if( fis == null ) { // Error while loading?
+        	System.err.println("Can't load file: '" + filename + "'");
+        	return;
+        }
+        
+        JFuzzyChart.get().chart(fis);        
+        
+        while ((fromServer = in.readLine()) != null) {
+        	StringTokenizer st = new StringTokenizer(fromServer);
+        	x = Double.valueOf(st.nextToken()).doubleValue();
+        	y = Double.valueOf(st.nextToken()).doubleValue();
+        	angle = Double.valueOf(st.nextToken()).doubleValue();
+
+        	System.out.println("x: " + x + " y: " + y + " angle: " + angle);
+        	
+			fis.setVariable("x", x);
+			fis.setVariable("y", y);
+			fis.setVariable("angle", angle);
+			
+			fis.evaluate();
+			
+			double respostaDaSuaLogica = fis.getVariable("output").getLatestDefuzzifiedValue();
+        	
+			System.out.println("\n" + respostaDaSuaLogica);
+			
+        	// envio da acao do volante
+        	out.println(respostaDaSuaLogica);
+        	
+            // requisicao da posicao do caminhao        	
+        	out.println("r");	
+        }
+ 
+        out.close();
+        in.close();
+        stdIn.close();
+        kkSocket.close();
+    }
+}
